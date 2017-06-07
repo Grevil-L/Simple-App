c8fd1912c238a6314993183e556bc58d460e0782
grevillopes <grevillopes@gmail.com>
2017-06-06 19:24:34 -0500

    added server and servers modules

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-servers', //tag or element selector
+  // selector: '[app-servers]',//attribute selector
+  // selector: '.app-servers', //class selector
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>`,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
