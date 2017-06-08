26dcd6639d11ed23b52a71b2a0a3e5ce8e5e889d
grevillopes <grevillopes@gmail.com>
2017-06-08 10:26:35 -0500

    added a button for creating new servers with a feedback message

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,17 +4,28 @@ import {Component, OnInit} from '@angular/core';
   selector: 'app-servers', //tag or element selector
   // selector: '[app-servers]',//attribute selector
   // selector: '.app-servers', //class selector
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No Server was created';
 
   constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 6000);
+
   }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'New Server was created';
   }
 
 }
